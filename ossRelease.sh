0e6f62028b2400d3ab172b507494f233d7951ccf
MORILLE Jérôme <morille.jerome@groupagrica.com>
2019-11-19 09:11:31 +0100

    Tag filter on Regex ^ossrh-.*$

---
+++ ossRelease.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+VERSION=${1:-0.0.1-SNAPSHOT}
+
+git co -b prepare-release
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$VERSION
+mvn clean install
+git commit -am "Prépare Release version $VERSION"
+git tag -a ossrh-$VERSION -m "Release version $VERSION"
+git push origin ossrh-$VERSION
+git checkout master
+git branch -D prepare-release
