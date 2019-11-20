ffeb55d55e9030996017de93af8a8304ead1cc09
ⓥⓘⓝⓒⓔ⑨ⓟⓐⓡⓐ <vdupain@gmail.com>
2019-11-20 15:51:14 +0100

    Update and rename ossRelease.sh to ossrh-release.sh

--- ossrh-release.sh
+++ ossrh-release.sh
@@ -5,7 +5,7 @@ VERSION=${1:-0.0.1-SNAPSHOT}
 git co -b prepare-release
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$VERSION
 mvn clean install
-git commit -am "Prépare Release version $VERSION"
+git commit -am "Prepare Release version $VERSION"
 git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
 git checkout master
